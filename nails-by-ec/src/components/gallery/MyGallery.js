d6b05003f2f6f5c7821b0cbd7472441ac0bb96d6
Lukas Czarniecki <lcse.2001@gmail.com>
2023-03-12 21:24:12 +0100

    working gallery with modal

--- nails-by-ec/src/components/gallery/MyGallery.js
+++ nails-by-ec/src/components/gallery/MyGallery.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './MyGallery.module.css';
 
 export const Gallery = props => {
+  const [modalUrl, setModalUrl] = useState('');
+
+  const handleModal = e => {
+    setModalUrl(e.currentTarget.src);
+
+    console.log(modalUrl);
+
+    document.querySelector('.modal').classList.add(`${classes.open}`);
+
+    // grab the image source !!!
+
+    // // populate the modal with ne info
+    // modalInner.innerHTML = `
+    //    <img width='600' height='600' src="${imgSrc.replace(
+    //      '200',
+    //      '600'
+    //    )}" alt="${name}"/>
+    //    <p>${desc}</p>
+    //   `;
+    //show the modal
+    // modalOuter.classList.add('open');
+  };
+
+  const closeModal = () => {
+    document.querySelector('.modal').classList.remove(`${classes.open}`);
+  };
+
   return (
     <>
       {props.images.map(image => (
-        <img src={image.url} alt={image.alt} style={image.style} />
+        <img
+          src={image.url}
+          alt={image.alt}
+          style={image.style}
+          key={image.alt}
+          onClick={handleModal}
+        />
       ))}
 
-      <div className={`${classes['modal-outer']} ${classes.open}`}>
-        <div className={classes['modal-inner']}></div>
+      <div className={`${classes['modal-outer']} modal`} onClick={closeModal}>
+        <div className={classes['modal-inner']}>
+          <img src={modalUrl} />
+        </div>
       </div>
     </>
   );
